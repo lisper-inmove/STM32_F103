d852d9edc207176d7a585c03681189fb694e2409
inmove <lisper.inmove@gmail.com>
2025-10-02 15:33:20 +0800

    Lesson 4

---
+++ main.c
@@ -0,0 +1,11 @@
+#include "stm32f1xx_hal.h"
+
+int main(void) {
+	HAL_Init();
+	HAL_RCC_MCOConfig(RCC_MCO1, RCC_MCO1SOURCE_SYSCLK, RCC_MCODIV_1);
+	SystemCoreClockUpdate();
+	uint32_t HCLKFreq = HAL_RCC_GetHCLKFreq();
+	uint32_t PCLK1Freq = HAL_RCC_GetPCLK1Freq();
+	uint32_t PCLK2Freq = HAL_RCC_GetPCLK2Freq();
+
+}
